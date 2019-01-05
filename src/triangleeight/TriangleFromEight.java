03a97352a40d116cc4341ab11981db4759ab616a
Vladislav Kuznecov <super.ewew2015@yandex.ru>
2019-01-05 21:08:48 +0300

    треугольник из 8ок

---
+++ src/triangleeight/TriangleFromEight.java
@@ -0,0 +1,13 @@
+package triangleeight;
+
+public class TriangleFromEight {
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(8);
+            }
+            System.out.println("");
+        }
+
+    }
+}
